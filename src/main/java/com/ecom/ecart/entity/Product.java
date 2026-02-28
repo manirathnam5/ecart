4ff111991c33df92544e0bc2ae6e0ef0ba05bfc3
manirathnam5 <manirathnam501@gmail.com>
2026-02-28 17:58:18 +0530

    added entity classes

--- src/main/java/com/ecom/ecart/entity/Product.java
+++ src/main/java/com/ecom/ecart/entity/Product.java
@@ -1,4 +1,117 @@
 package com.ecom.ecart.entity;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    @NotBlank(message = "Name field is required")
+    private String name;
+    @Column(nullable = false)
+    @NotBlank(message = "Price field is required")
+    private Double price;
+    //@Column(nullable = true)
+    @NotBlank(message = "Description field is required")
+    private String description;
+
+    private Double rating = 0.0;
+    @NotBlank(message = "Seller field is required")
+    private String seller;
+    @NotBlank(message = "Stock field is required")
+    private Integer stock;
+
+    private Integer noOfReviews = 0;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name="product_id")
+    private List<ProductImage> productImageList;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "product_id")
+    private List<ProductReview> productReviewList;
+
+
+    public Product(Long id, String name, Double price, String description, Double rating, String seller, Integer stock, Integer noOfReviews) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.rating = rating;
+        this.seller = seller;
+        this.stock = stock;
+        this.noOfReviews = noOfReviews;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getNoOfReviews() {
+        return noOfReviews;
+    }
+
+    public void setNoOfReviews(Integer noOfReviews) {
+        this.noOfReviews = noOfReviews;
+    }
 }
