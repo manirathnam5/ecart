3e6fca65fc94b2b4dc4ea99f534bfa78e43ae132
manirathnam5 <manirathnam501@gmail.com>
2026-03-11 13:03:16 +0530

    controller code changes

--- src/main/java/com/ecom/ecart/controller/ProductController.java
+++ src/main/java/com/ecom/ecart/controller/ProductController.java
@@ -1,15 +1,27 @@
 package com.ecom.ecart.controller;
 
+import com.ecom.ecart.entity.Product;
+import com.ecom.ecart.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/product")
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product/")
 public class ProductController {
 
 
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/list")
-    public String getProductList(){
-        return "Product Lists";
+    public ResponseEntity<List<Product>> getProductList() {
+        List<Product> productList = productService.getAllProducts();
+        return ResponseEntity.ok(productList);
     }
+
 }
